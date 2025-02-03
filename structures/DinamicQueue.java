60bb1d89e7f5c1584fa43a54b72c449af7b3778d
Willyam Mach <willyam.mach@gmail.com>
2025-02-03 14:00:36 -0300

    criando fila dinâmica + ajuste de sintaxe

---
+++ structures/DinamicQueue.java
@@ -0,0 +1,44 @@
+public class DinamicQueue<T> {
+    private Node<T> start;
+    private Node<T> end;
+
+    public DinamicQueue() {
+        this.start = null;
+        this.end = null;
+    }
+
+    public void add(T element) {
+        Node<T> newNode = new Node<>(element);
+
+        if (this.end == null) {
+            this.start = newNode;
+            this.end = newNode;
+            return;
+        } 
+        
+        this.end.setNext(newNode);
+        this.end = newNode;
+    }
+
+    public void pop() {
+        if (this.start == null) {
+            System.out.println("Nothing to see here");
+            return;
+        }
+        
+        this.start = this.start.getNext();
+
+        if (this.start == null) {
+            this.end = null;
+        }
+    }
+
+    public void showValues() {
+        Node<T> aux = this.start;
+
+        while (aux != null) {
+            System.out.println(aux.getElement());
+            aux = aux.getNext();
+        }
+    }
+}
